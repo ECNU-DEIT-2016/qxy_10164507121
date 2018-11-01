ada95a176c4c12593246db0b43d0eae17273c874
q707132127 <707132127@qq.com>
2018-11-01 13:34:05 +0800

    第二次作业随机点名V4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
 ## 1.0.0
 
 - Initial version, created by Stagehand
+
+## 第三次作业
+
+- feature 1 实现基本随机点名
+- feature 2 打印班级列表
+- feature 3 增加回答评分功能
+- feature 4 增加学生
--- lib/src/random_rollcall/backend_service.dart
+++ lib/src/random_rollcall/backend_service.dart
@@ -4,24 +4,18 @@ import 'student.dart';
 
 class BackendService {
   static final _mockStudents = [
-    Student('77'),
-    Student('菜鸡'),
-    Student('康康'),
-    Student('翔酱'),
-    Student('女神'),
-    Student('张杰'),
-    Student('朱一龙'),
-    Student('白宇'),
-    Student('林俊杰'),
-    Student('松二'),
-    Student('自然醒'),
-    Student('梁乐'),
-    Student('真源'),
-    Student('kiki'),
-    Student('阴酱'),
-    Student('静雅'),
-    Student('雪怡'),
-    Student('小鸡')
+    Student('戚晓颖'),
+    Student('蔡心蕊'),
+    Student('李典康'),
+    Student('周嘉翔'),
+    Student('龙晶毅'),
+    Student('唐莉雯'),
+    Student('吴松二'),
+    Student('张静雅'),
+    Student('陈瑶'),
+    Student('郑可欣'),
+    Student('朱子恒'),
+    Student('赵世宇'),
   ];
 
   Future<List> getAll(type) async => type == Student
