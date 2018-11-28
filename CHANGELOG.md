4326811632a2915572ed1d901d956be09e9311b6
q707132127 <707132127@qq.com>
2018-11-28 23:24:36 +0800

    服务器端随机点名

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,11 @@
 - feature 2 打印班级列表
 - feature 3 增加回答评分功能
 - feature 4 增加学生
+
+##第四次作业
+
+端口号：8002
+- feature 1 实现随机点名方法
+- feature 2 localhost:8002/random 指向随机点名
+- feature 3 localhost:8002/users 指向班级全部人员列表
+- feature 4 localhost:8002/users/[:id] 指向列表中第id个人
--- my_server/lib/users.dart
+++ my_server/lib/users.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 
 class Users {
     static List<String> things = ['戚晓颖', '蔡心蕊', '李典康','周嘉翔', '龙晶毅', '唐莉雯','吴松二', '张静雅','陈瑶', '郑可欣','朱子恒', '赵世宇'];
-    static String randomThing;
+  static String randomThing;
     static String read(){
       var random = Random();
         randomThing =things[random.nextInt(12)];
